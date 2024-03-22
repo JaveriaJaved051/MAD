2d6297d01a104d5e69e8e9fa8da4fde020ba50a5
JaveriaJaved(051) <150766187+JaveriaJaved051@users.noreply.github.com>
2024-03-22 20:07:50 +0500

    adding tasks

--- LabAssignments/Lab5Tasks/App.js
+++ LabAssignments/Lab5Tasks/App.js
@@ -5,8 +5,7 @@
 //  const [getCount, setCount] = useState(0);
 //  return (
 //  <View style={styles.container}>
-//  <Text style={{fontSize:18, fontWeight:'bold'}}>Counter 
-// Application</Text>
+//  <Text style={{fontSize:18, fontWeight:'bold'}}>Counter Application</Text>
 //  <View style={styles.just} flexDirection='row'>
 //  <Button title='Decrease' color='red' 
 // onPress={()=>{setCount(getCount-1)}} disabled={getCount==0}></Button>
@@ -33,13 +32,16 @@
 
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View,Text,Button, StyleSheet } from 'react-native';
 import GameLayout from './GameLayout'; // Assuming GameLayout.js is in the same directory
 import LoginScreen from './LoginScreen'; // Assuming LoginScreen.js is in the same directory
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Button color={Red} onPress={()=>{console.log("Button Pressed")}}>I am red Buttom</Button>
+      <Text style={styles.container}>Again Wecome Javeria This is for you to learn</Text>
+     <Button color={red}>"Stop makig more Butttons"</Button>
       <GameLayout/>
        <LoginScreen />
 
